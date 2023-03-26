82239668e7023c2db140d44ce2dd3099cd7c7be0
Cory Zastoupil <coryzast@gmail.com>
2023-03-26 01:14:08 -0400

    Spiffied up navbar, made black

--- frontend/.next/static/webpack/webpack.ad5aac3453640a52.hot-update.js
+++ frontend/.next/static/webpack/webpack.ad5aac3453640a52.hot-update.js
@@ -33,7 +33,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a6e1eed3a869840f"; }
+/******/ 	__webpack_require__.h = function() { return "7b20b2126910c51b"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/jsonp chunk loading */
