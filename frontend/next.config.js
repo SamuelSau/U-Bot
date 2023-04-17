57522d514a844cfaca32362f7883ebd19e3db9f2
Samuel Sau <samuel51@usf.edu>
2023-04-17 10:29:24 -0400

    added pages to root directory of frontend folder

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pagesDir: 'frontend/src/pages'
 }
 
 module.exports = nextConfig
---
+++ frontend/pages/_app.js
@@ -0,0 +1,6 @@
+import '@/styles/globals.css'
+import 'semantic-ui-css/semantic.min.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
